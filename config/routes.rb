e58603a3ed34227e3d5bdb23549ae684aa6500c5
Anastasiya Kom <young.ru96@mail.ru>
2021-01-18 22:02:09 +0300

    set a default sender/uncomment sign in counts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accountsse
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #передаём GET запрос на адрес/ методу ... контроллера RootController
   root to: "public#homepage"
