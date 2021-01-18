02daf3fb0ab7f44228855dbf24b12835afc0597f
Anastasiya Kom <young.ru96@mail.ru>
2021-01-18 22:15:39 +0300

    run migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :accountsse
+  devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #передаём GET запрос на адрес/ методу ... контроллера RootController
   root to: "public#homepage"
